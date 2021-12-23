eae4ce253814d7beeec1e810f0240f5c088e3318
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-23 23:54:53 +0100

    Implementation of fixes for Firebase versioning.

--- mess_app/lib/screens/chat_screen.dart
+++ mess_app/lib/screens/chat_screen.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(8),
-                child: const Text('This works!'),
-              )),
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('This works!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/Pzomcb9eKEAr2liDejwe/messages')
+              .snapshots()
+              .listen((data) {
+            print(data.documents[0]['text']);
+          });
+        },
+      ),
     );
   }
 }
