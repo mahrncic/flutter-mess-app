6853df28ff7407645f0d802e82ebf7621ebd1175
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-24 10:50:41 +0100

    Added initial dummy authentication form.

--- mess_app/lib/main.dart
+++ mess_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mess_app/screens/auth_screen.dart';
 import 'package:mess_app/screens/chat_screen.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
---
+++ mess_app/lib/widgets/auth/auth_form.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: 'Email Address',
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Username'),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  RaisedButton(
+                    child: const Text('Login'),
+                    onPressed: () {},
+                  ),
+                  FlatButton(
+                    child: const Text('Create new account'),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
