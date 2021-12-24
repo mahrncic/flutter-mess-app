deff37adbd3fe5f2cd0890c662386ef69c7daa28
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-24 18:22:56 +0100

    Complete Firebase authentication functionality.

--- mess_app/lib/main.dart
+++ mess_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mess_app/screens/auth_screen.dart';
 import 'package:mess_app/screens/chat_screen.dart';
@@ -26,7 +27,15 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.hasData) {
+            return ChatScreen();
+          }
+          return AuthScreen();
+        },
+      ),
     );
   }
 }
