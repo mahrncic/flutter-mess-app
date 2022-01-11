57153880a547cf25d4d97404273c858dff983eab
Marko Hrncic <marko.hrncic@kogitas.com>
2022-01-11 22:28:01 +0100

    Separating body from the main Settings screen based on the current architecture.

--- mess_app/lib/screens/settings_screen.dart
+++ mess_app/lib/screens/settings_screen.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mess_app/constants/constants.dart';
 import 'package:mess_app/screens/login_screen.dart';
 import 'package:mess_app/services/auth.dart';
+import 'package:mess_app/widgets/settings/body.dart';
 
 class SettingsScreen extends StatefulWidget {
   @override
@@ -33,60 +32,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
         ],
       ),
-      body: Container(
-        padding: const EdgeInsets.only(left: 16, top: 25, right: 16),
-        child: ListView(
-          children: [
-            Row(
-              children: const [
-                Icon(
-                  Icons.volume_up_outlined,
-                  color: kPrimaryColor,
-                ),
-                SizedBox(
-                  width: 8,
-                ),
-                Text(
-                  "Overall",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            const Divider(
-              height: 15,
-              thickness: 2,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            buildNotificationOptionRow("Dark Mode", false),
-            buildNotificationOptionRow("Fingerprint Lock Screen", false),
-            buildNotificationOptionRow("Notifications", false),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Row buildNotificationOptionRow(String title, bool isActive) {
-    bool _isActive = isActive;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          title,
-          style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-              color: Colors.grey[600]),
-        ),
-        Transform.scale(
-            scale: 0.7,
-            child: Switch(
-              value: _isActive,
-              onChanged: (bool val) {},
-            ))
-      ],
+      body: Body(),
     );
   }
 }
